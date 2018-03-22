57483d730fbc1a87b8c43434ae1f3fa4626f873e
Usama Sajid <usanasm@yahoo.com>
2018-03-22 00:41:58 -0400

    methods unloaded

--- homework6.scm
+++ homework6.scm
@@ -16,14 +16,14 @@
 ;; number 2
 ;;
 
-;;(define (tribonacci n)
+(define (tribonacci n)
 
-;;   (cond
-;;     ((zero? n) 0)
-;;     ((= n 1) 0)
-;;     ((= n 2) 0)
-;;     ((= n 3) 1)
-;;     (else (+ (tribonacci (- n 1) ) (+ (tribonacci (- n 2)) (tribonacci (- n 3) ))))))
+   (cond
+     ((zero? n) 0)
+     ((= n 1) 0)
+     ((= n 2) 0)
+     ((= n 3) 1)
+     (else (+ (tribonacci (- n 1) ) (+ (tribonacci (- n 2)) (tribonacci (- n 3) ))))))
 
 ;; == test == 
 ;;(tribonacci 9)
