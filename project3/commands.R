47030942347c8d79a800aa06eb1c2ce84d600938
usamasajid123 <usanasm@yahoo.com>
2018-05-15 17:36:41 -0400

    fixing

--- project3/commands.R
+++
@@ -1,6 +0,0 @@
-# the R commands in this file produce a pdf file, named pageshist.pdf of 
-# the numbers in the file named data
-pdf(file="pageshist.pdf")
-d<-read.table("data")
-attach(d)
-hist(V1)
